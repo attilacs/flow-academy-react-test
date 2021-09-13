0ffdf34c6b7901afd489077a28609080959e3f28
Attila Csanyi <csanyiattila23@gmail.com>
2021-09-13 11:30:08 +0200

    Add props to Values

--- src/App.js
+++ src/App.js
@@ -2,10 +2,12 @@ import axios from 'axios';
 import { useEffect, useState } from 'react';
 import './App.css';
 import ForintInput from './Components/ForintInput';
+import Values from './Components/Values';
 
 function App() {
   const [forint, setForint] = useState(0);
   const [exchangeRate, setExchangeRate] = useState({});
+
   useEffect(() => {
     const fetchExchange = async () => {
       try {
@@ -13,19 +15,33 @@ function App() {
           'https://api.exchangerate.host/latest?base=huf'
         );
         setExchangeRate({
-          usd: response.data.rates.usd,
-          eur: response.data.rates.eur
+          usd: response.data.rates.USD,
+          eur: response.data.rates.EUR
         });
+        console.log(response);
       } catch (err) {
         console.error(err);
       }
     };
     fetchExchange();
   }, []);
+
   return (
     <div className="App container">
-      <h2>Deviza váltás</h2>
-      <ForintInput setForint={setForint} forint={forint} />
+      <div className="row">
+        <h2>Deviza váltás</h2>
+        <div className="col"></div>
+      </div>
+      <div className="row">
+        <div className="col">
+          <ForintInput setForint={setForint} forint={forint} />
+        </div>
+      </div>
+      <div className="row">
+        <div className="col d-flex justify-content-center">
+          <Values forint={forint} exchangeRate={exchangeRate} />
+        </div>
+      </div>
     </div>
   );
 }
--- src/Components/Values.js
+++ src/Components/Values.js
@@ -1,5 +1,12 @@
-const Values = () => {
-  return <div></div>;
+const Values = ({ forint, exchangeRate }) => {
+  return (
+    <div className="d-flex flex-column">
+      <h3>
+        {forint} HUF = {exchangeRate.eur} EUR
+      </h3>
+      <h3>usd</h3>
+    </div>
+  );
 };
 
 export default Values;
