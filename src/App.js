3922e1a005ec529ac60f6e2208f5f9e6461ee64f
Attila Csanyi <csanyiattila23@gmail.com>
2021-09-13 11:15:27 +0200

    Add useEffect hook

--- src/App.js
+++ src/App.js
@@ -1,9 +1,26 @@
 import axios from 'axios';
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 import './App.css';
 import ForintInput from './Components/ForintInput';
 
 function App() {
+  const [exchangeRate, setExchangeRate] = useState({});
+  useEffect(() => {
+    const fetchExchange = async () => {
+      try {
+        const response = await axios.get(
+          'https://api.exchangerate.host/latest?base=huf'
+        );
+        setExchangeRate({
+          usd: response.data.rates.usd,
+          eur: response.data.rates.eur
+        });
+      } catch (err) {
+        console.error(err);
+      }
+    };
+    fetchExchange();
+  }, []);
   return (
     <div className="App container">
       <h2>Deviza váltás</h2>
