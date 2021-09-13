bbfd931078bcb892bcfa572986362797632547aa
Attila Csanyi <csanyiattila23@gmail.com>
2021-09-13 11:07:18 +0200

    Add useState to ForintInput component

--- src/Components/ForintInput.js
+++ src/Components/ForintInput.js
@@ -1,7 +1,21 @@
+import { useState } from 'react';
+
 const ForintInput = () => {
+  const [forint, setForint] = useState(0);
   return (
     <form>
-      <label htmlFor="forint">Forint (HUF)</label>
+      <div className="mb-3">
+        <label className="form-label" htmlFor="forint">
+          Forint (HUF)
+        </label>
+        <input
+          className="form-control"
+          type="number"
+          name="forint"
+          value={forint}
+          onChange={(e) => setForint(e.target.value)}
+        />
+      </div>
     </form>
   );
 };
